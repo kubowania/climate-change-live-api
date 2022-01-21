5a67c033003aa0bdbff87e2a728e6ac6c4456716
Shanker Sidambaram <s.sidambaram@sana-commerce.com>
2022-01-21 16:37:19 +0530

    fina commit

--- index.js
+++ index.js
@@ -4,72 +4,24 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-const newspapers = [
+const newspapers = [   
     {
-        name: 'cityam',
-        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        name: 'Daily Mirror',
+        address: 'https://www.dailymirror.lk/latest-news/108',
         base: ''
     },
     {
-        name: 'thetimes',
-        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        name: 'Island',
+        address: 'https://island.lk/category/news/',
         base: ''
     },
     {
-        name: 'guardian',
-        address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: '',
-    },
-    {
-        name: 'telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk',
-    },
-    {
-        name: 'nyt',
-        address: 'https://www.nytimes.com/international/section/climate',
-        base: '',
-    },
-    {
-        name: 'latimes',
-        address: 'https://www.latimes.com/environment',
-        base: '',
-    },
-    {
-        name: 'smh',
-        address: 'https://www.smh.com.au/environment/climate-change',
-        base: 'https://www.smh.com.au',
-    },
-    {
-        name: 'un',
-        address: 'https://www.un.org/climatechange',
-        base: '',
-    },
-    {
-        name: 'bbc',
-        address: 'https://www.bbc.co.uk/news/science_and_environment',
-        base: 'https://www.bbc.co.uk',
-    },
-    {
-        name: 'es',
-        address: 'https://www.standard.co.uk/topic/climate-change',
-        base: 'https://www.standard.co.uk'
-    },
-    {
-        name: 'sun',
-        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
-        base: ''
-    },
-    {
-        name: 'dm',
-        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
-        base: ''
-    },
-    {
-        name: 'nyp',
-        address: 'https://nypost.com/tag/climate-change/',
+        name: 'Adaderana',
+        address: 'http://www.adaderana.lk/hot-news',
         base: ''
     }
+
+
 ]
 
 const articles = []
@@ -80,7 +32,7 @@ newspapers.forEach(newspaper => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {
+            $('a:contains("coronavirus"), a:contains("covid"), a:contains("Shani")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
